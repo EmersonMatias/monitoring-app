306ea9bb6c7a2d0d778ee20613025de93253a70a
Emerson <emersonsmatthias@gmail.com>
2023-12-26 21:22:58 -0300

    teste

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -19,7 +19,39 @@ chegou: false/true
 */
 
 export default async function Inicio() {
-    const data = await getData()
+    const data = [
+        {
+            "name": "Emerson Rodrigo dos Santo",
+            "hour": "14:20",
+            "agency": "Agência Bancária",
+            "arrived": true
+    
+        },
+        {
+            "name": "Adriana Jovenato",
+            "hour": "11:20",
+            "agency": "Agência Correios",
+            "arrived": false
+        },
+        {
+            "name": "Marta Maria",
+            "hour": "15:20",
+            "agency": "Agência Correios",
+            "arrived": true
+        },
+        {
+            "name": "Marcos Maria",
+            "hour": "18:20",
+            "agency": "Agência Correios",
+            "arrived": false
+        },
+        {
+            "name": "Pedro Fontes",
+            "hour": "23:30",
+            "agency": "Agência Internacional",
+            "arrived": false
+        }
+    ] 
     const date = new Date()
     const currentHour = date.getHours().toString()
     const currentMinutes = date.getMinutes().toString()
