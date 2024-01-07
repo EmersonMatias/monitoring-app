84ed819c6455078609a5f7a1c23729b1ba4d4e5f
Emerson <emersonsmatthias@gmail.com>
2024-01-07 12:38:15 -0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env:{
-        BACKEND_URL: "http://monitoring-server.onrender.com"
+        BACKEND_URL: "https://monitoring-server.onrender.com"
     }
 }
 
