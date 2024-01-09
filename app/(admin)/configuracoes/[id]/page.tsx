b9b2c9ea1a117a7ed33de5d8ec1f9da6885ae6eb
Emerson <emersonsmatthias@gmail.com>
2024-01-09 19:34:31 -0300

    deploy

--- app/(admin)/configuracoes/[id]/page.tsx
+++ app/(admin)/configuracoes/[id]/page.tsx
@@ -11,7 +11,8 @@ export default function EditVigilant({ params }: { params: { id: string } }) {
             const data: AxiosResponse<TGetUserForUpdate> = await axios.get(`${process.env.BACKEND_URL}/vigilantwithstatus=${params.id}`)
             return data.data
         },
-        refetchOnMount: true
+        refetchOnMount: true,
+        enabled: params.id.length !== 0
     })
 
     const initialData = {
@@ -99,7 +100,157 @@ export default function EditVigilant({ params }: { params: { id: string } }) {
             <div className="px-20 flex justify-between items-center font-bold">
                 <h2 className="text-3xl">Editar vigilante</h2>
             </div>
-        
+            {isSuccess &&
+                <div className={`w-[600px] h-[700px] text-[#0b0b0b] bg-white mt-10 p-12 overflow-y-scroll ${styles.scrollable}`}>
+                    <form className="flex flex-col" onSubmit={(event) => { handleSubmite(event) }}>
+                        <label htmlFor="name" className="text-base mb-2 font-bold">Nome Completo:</label>
+                        <input
+                            type="text"
+                            minLength={3}
+                            maxLength={150}
+                            disabled={isPending}
+                            id="name"
+                            placeholder="Digite o nome completo"
+                            className={styleInput}
+                            required
+                            value={updateVigilantData.name}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, name: event.target.value }))}
+                        />
+
+                        <label htmlFor="date" className=" text-base mb-2 font-bold">Data de Nascimento:</label>
+                        <input
+                            type="date"
+                            disabled={isPending}
+                            id="date"
+                            minLength={10}
+                            maxLength={10}
+                            placeholder="Digite a data de nascimento"
+                            className={styleInput}
+                            required
+                            value={updateVigilantData.dateofbirth}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, dateofbirth: event.target.value }))}
+                        />
+
+                        <label htmlFor="rg" className="text-base mb-2 font-bold">RG:</label>
+                        <input
+                            type="text"
+                            disabled={isPending}
+                            id="rg"
+                            placeholder="Digite o RG - Apenas números sem pontos ou espaço "
+                            className={styleInput}
+                            required
+                            minLength={9}
+                            maxLength={9}
+                            value={updateVigilantData.rg}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, rg: event.target.value.replace(/\D/g, '') }))}
+                        />
+
+                        <label htmlFor="cpf" className="text-base mb-2 font-bold">CPF:</label>
+                        <input
+                            type="text"
+                            disabled={isPending}
+                            id="cpf"
+                            placeholder="Digite o CPF - Apenas números sem pontos ou espaço "
+                            className={styleInput}
+                            required
+                            minLength={11}
+                            maxLength={11}
+                            value={updateVigilantData.cpf}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, cpf: event.target.value.replace(/\D/g, '') }))}
+
+                        />
+
+                        <label htmlFor="agencia" className="text-base mb-2 font-bold">Agência:</label>
+                        <input
+                            type="text"
+                            disabled={isPending}
+                            id="agencia"
+                            min={5}
+                            className={styleInput}
+                            required
+                            value={updateVigilantData.agency}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, agency: event.target.value }))}
+                        />
+
+                        <label htmlFor="horariodeentrada" className="text-base mb-2 font-bold">Horário de Entrada:</label>
+                        <input
+                            type="time"
+                            disabled={isPending}
+                            id="horariodeentrada"
+                            className={styleInput}
+                            required
+                            minLength={5}
+                            maxLength={5}
+                            value={updateVigilantData.entryTime}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, entryTime: event.target.value }))}
+                        />
+
+                        <label htmlFor="horariodesaida" className="text-base mb-2 font-bold">Horário de Saída:</label>
+                        <input
+                            type="time"
+                            disabled={isPending}
+                            id="horariodesaida"
+                            className={styleInput}
+                            required
+                            value={updateVigilantData.departureTime}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, departureTime: event.target.value }))}
+                        />
+
+                        <label htmlFor="frequency" className="text-base mb-2 font-bold">Frequência de comunicação:</label>
+                        <input
+                            type="number"
+                            id="frequency"
+                            disabled={isPending}
+                            placeholder="Digite a frequência de cominucação em minutos "
+                            className={styleInput}
+                            minLength={5}
+                            maxLength={150}
+                            required
+                            value={updateVigilantData.frequency}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, frequency: Number(event.target.value) }))}
+                        />
+
+
+                        <label htmlFor="login" className="text-base mb-2 font-bold">Login:</label>
+                        <input
+                            type="text"
+                            id="login"
+                            disabled={isPending}
+                            placeholder="Digite o login "
+                            className={styleInput}
+                            minLength={5}
+                            maxLength={150}
+                            required
+                            value={updateVigilantData.login}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, login: event.target.value }))}
+                        />
+
+                        <label htmlFor="senha" className="text-base mb-2 font-bold">Senha:</label>
+                        <input
+                            type="password"
+                            id="senha"
+                            placeholder="Digite a senha "
+                            minLength={8}
+                            maxLength={150}
+                            className="pl-4 py-2 bg-[#fdd28846] rounded-xl mb-6 disabled:opacity-50"
+                            required
+                            value={updateVigilantData.password}
+                            onChange={(event) => (setUpdateVigilantData({ ...updateVigilantData, password: event.target.value }))}
+                            disabled={isPending}
+                        />
+
+                        <button className="bg-[#f0a830] py-3 mx-[150px] text-xl font-semibold rounded-md text-white disabled:opacity-50 " disabled={isPending}>{isPending ? "Atualizando usuário..." : "Atualizar usuário"}</button>
+                    </form>
+
+                    <div className={`fixed font-bold text-2xl bottom-10 right-10 bg-green-500 bg-opacity-70 p-8 rounded-lg ${sucessMessage ? "" : "hidden"}`} >
+                        Conta atualizada com sucesso.
+                    </div>
+
+                    <div className={`max-w-80 fixed text-center font-bold text-2xl bottom-10 right-10 bg-red-500 bg-opacity-70 p-8 rounded-lg ${errorMessage ? "" : "hidden"}`} >
+                        Conta não atualizada! Verifique os dados inseridos.
+                    </div>
+                </div>
+            }
         </div>
     )
 }
