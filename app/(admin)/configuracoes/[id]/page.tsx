2d35c80c5ae863319144f03be9e0e9db369fc19f
Emerson <emersonsmatthias@gmail.com>
2024-01-09 19:28:19 -0300

    deploy

--- app/(admin)/configuracoes/[id]/page.tsx
+++ app/(admin)/configuracoes/[id]/page.tsx
@@ -34,29 +34,28 @@ export default function EditVigilant({ params }: { params: { id: string } }) {
     const styleInput = "pl-4 py-2 bg-[#fdd28846] rounded-xl mb-6 disabled:opacity-50"
 
     useEffect(() => {
-        const tes = () => {
-            if (isSuccess) {
-                const birthday = vigilant?.dateofbirth?.split("/")
-
-                setUpdateVigilantData({
-                    name: vigilant?.name,
-                    agency: vigilant?.agency,
-                    cpf: vigilant?.cpf,
-                    rg: vigilant?.rg,
-                    entryTime: vigilant?.entryTime,
-                    departureTime: vigilant?.departureTime,
-                    dateofbirth: `${birthday[2]}-${birthday[1]}-${birthday[0]}`,
-                    login: vigilant?.login,
-                    password: "",
-                    frequency: vigilant?.status[0]?.frequency
-                })
-            }
-        }
         if (typeof window !== 'undefined') {
+            const tes = () => {
+                if (isSuccess) {
+                    const birthday = vigilant?.dateofbirth?.split("/")
+
+                    setUpdateVigilantData({
+                        name: vigilant?.name,
+                        agency: vigilant?.agency,
+                        cpf: vigilant?.cpf,
+                        rg: vigilant?.rg,
+                        entryTime: vigilant?.entryTime,
+                        departureTime: vigilant?.departureTime,
+                        dateofbirth: `${birthday[2]}-${birthday[1]}-${birthday[0]}`,
+                        login: vigilant?.login,
+                        password: "",
+                        frequency: vigilant?.status[0]?.frequency
+                    })
+                }
+            }
+
             tes()
         }
-
-
 
     }, [isSuccess, vigilant, updateVigilantData.name])
 
