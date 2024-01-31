2d36489ee086c730b73a2affc335c4c9befcfc72
Emerson <emersonsmatthias@gmail.com>
2024-01-31 18:13:26 -0300

    deploy

--- app/(admin)/contingencia/page.tsx
+++ app/(admin)/contingencia/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { dateTime } from "@/app/utils/constants"
 import { useGetAllContingency } from "@/hooks/hooks-contingency"
-import { useEffect, useRef } from "react"
+import { useRef } from "react"
 
 export default function Contingency() {
     const { data: contingency, isRefetching } = useGetAllContingency()
@@ -21,15 +21,40 @@ export default function Contingency() {
             return <div className="bg-[#FC6F6F] text-base p-2 mb-4 font-bold text-center rounded-lg text-white"> ATRASADO </div>
         }
     }
+    if (typeof window !== 'undefined') {
 
-    useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
             const pos = position.coords
             testeRef.current = pos
             return pos
         }, null, { enableHighAccuracy: true })
-    }, [])
+    }
 
+
+    function haversine(lat1: number, lon1: number, lat2: number | undefined, lon2: number | undefined) {
+        const R = 6371; // Raio médio da Terra em km
+
+        const dlat = toRadians(Number(lat2) - lat1);
+        const dlon = toRadians(Number(lon2) - lon1);
+
+        const a = Math.sin(dlat / 2) ** 2 + Math.cos(toRadians(lat1)) * Math.cos(toRadians(Number(lat2))) * Math.sin(dlon / 2) ** 2;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        const distance = (R * c) * 1000; // Distância em km
+        return distance;
+    }
+
+    function toRadians(degrees: number) {
+        return degrees * (Math.PI / 180);
+    }
+
+    const lat1 = -21.1175121;
+    const lon1 = -47.8167268;
+
+    const distance = haversine(lat1, lon1, testeRef.current?.latitude, testeRef.current?.longitude);
+    console.log(testeRef)
+
+    console.log(`Distância entre os pontos: ${distance.toFixed(2)} m`);
 
     return (
         <main className="flex flex-col items-center">
@@ -62,8 +87,9 @@ export default function Contingency() {
                 </tbody>
             </table>
             <div>
-                <p>Latitude: {testeRef.current?.latitude}</p>
-                <p>Longitude: {testeRef.current?.longitude}</p>
+                <p>Latitude atual: {testeRef.current?.latitude}</p>
+                <p>Longitude atual: {testeRef.current?.longitude}</p>
+                <p>Distância entre os pontos: {distance.toFixed(2)} m</p>
             </div>
         </main>
     )
