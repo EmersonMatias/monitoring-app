63058f17c54c9f9138f0db1879917d1d0810c625
Emerson <emersonsmatthias@gmail.com>
2024-02-17 19:14:37 -0300

    refetch: status

--- app/vigilante/Status.tsx
+++ app/vigilante/Status.tsx
@@ -39,17 +39,51 @@ export default function Status() {
     const lastTimeStatus = isSuccess && new Date(status?.timestamp).getTime()
     const currentTime = new Date().getTime()
     const diff = Math.floor((currentTime - Number(lastTimeStatus)) / 60000)
-    const late = diff > range
+
+    const makeCheckpoint = diff >= range
+    const late = diff > (range+5)
+    const minutesAfterCheckpoint = diff <= (range+5)
+    const minutesRest = (range+5)-diff
+
+
     const afterPanic = status?.situation === "PANIC" && diff < 1
+    const minutesAfterPanic = 6 - diff
+    const afterPanicRange = status?.situation === "PANIC" && diff < 6
 
+
+    console.log(minutesAfterCheckpoint)
+    
     function statusMessage() {
-        if (afterPanic) return <p className="text-center font-bold">Você ainda não pode fazer sua atualização do status. Falta 1 minuto</p>
-        if (status?.situation === "PANIC") return <p className="text-center">Você ja pode fazer a marcação de status</p>
+        if (afterPanic) return <p className="text-center font-bold bg-gray-200 p-2 rounded-md text-red-500">Você ainda não pode fazer sua atualização do status. Falta 1 minuto</p>
+        if (afterPanicRange) return (
+            <>
+                <p className="text-center font-bold text-blue-500 bg-gray-200 p-2 rounded-md">Você já pode fazer a marcaçaõ de status.</p>
+                <p className="text-orange-600 font-semibold">Você tem {minutesAfterPanic} minutos, após isso, uma notificaçaõ será gerada ao adminsitrador!</p>
+            </>
+        )
+        if (status?.situation === "PANIC") return (
+            <>
+                <p className="text-center bg-gray-200 p-2 rounded-md font-bold text-blue-500">Você ja pode fazer a marcação de status.</p>
+                <p className="text-red-500 font-semibold">Você está atrasado! Uma notificaçao foi gerada ao adminsitrador.</p>
+            </>
+        )
 
-        if (!late) {
-            return <p className="text-center font-bold">Você ainda não pode fazer sua atualização do status. Faltam {isSuccess ? range - diff : ""} minutos</p>
+        if (!makeCheckpoint) {
+            return <p className="text-center font-bold bg-gray-200 p-2 rounded-md text-red-500">Você ainda não pode fazer sua atualização do status. Faltam {isSuccess && range - diff} minutos</p>
+        } else if (minutesAfterCheckpoint) {
+            return (
+                <>
+                    <p className="text-center font-bold text-blue-500 bg-gray-200 p-2 rounded-md">Você já pode fazer a marcaçaõ de status.</p>
+                    <p className="text-orange-600 font-semibold">Você tem {minutesRest} minutos, após isso, uma notificaçaõ será gerada ao adminsitrador!</p>
+                </>
+            )
         } else if (late) {
-            return <p className="text-center">Você ja pode fazer a marcação de status</p>
+            return (
+                <>
+                <p className="text-center bg-gray-200 p-2 rounded-md font-bold text-blue-500">Você ja pode fazer a marcação de status.</p>
+                <p className="text-red-500 font-semibold">Você está atrasado! Uma notificaçao foi gerada ao adminsitrador.</p>
+            </>
+            )
         }
     }
 
@@ -62,7 +96,7 @@ export default function Status() {
                 {statusMessage()}
 
                 <div className="flex items-center flex-col gap-2 mt-4 ">
-                    <button disabled={status?.situation === "PANIC" ? afterPanic : !late} className="p-2 bg-blue-500 rounded-md text-white font-bold max-w-[250px] min-w-[250px] disabled:opacity-50" onClick={handleOkay}>
+                    <button disabled={status?.situation === "PANIC" ? afterPanic : !makeCheckpoint} className="p-2 bg-blue-500 rounded-md text-white font-bold max-w-[250px] min-w-[250px] disabled:opacity-50" onClick={handleOkay}>
                         STATUS OK
                     </button>
 
